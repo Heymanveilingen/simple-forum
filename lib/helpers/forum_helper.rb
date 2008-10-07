2aa404554bca273353b1b7a235d2bfe37fea3de0
Ricardo Markiewicz <gazer.arg@gmail.com>
2008-10-07 16:54:32 -0300

    Move the last poster information to the helper and cleanup the views

--- lib/helpers/forum_helper.rb
+++ lib/helpers/forum_helper.rb
@@ -8,4 +8,12 @@ module ForumHelper
       yield
     end
   end
+
+  def last_post_info(object)
+    s = []
+    s << object.last_post_at.strftime("%d-%m-%Y %H:%M") if object.last_post_at
+    s << "por" if object.last_post_by
+    s << link_to(h(object.last_poster.name), object.last_poster) if object.last_poster
+    s * "&nbsp;"
+  end
 end
