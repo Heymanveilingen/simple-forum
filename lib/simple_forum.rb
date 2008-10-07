f7513b666d0131fed996ee4bcff818f22f5cd698
Ricardo Markiewicz <gazer.arg@gmail.com>
2008-10-07 00:19:22 -0300

    Include the new helper in the initialization chain

--- lib/simple_forum.rb
+++ lib/simple_forum.rb
@@ -3,6 +3,7 @@ require "models/forum_topic"
 require "models/forum_post"
 
 require 'forum_session'
+require 'helpers/forum_helper'
 
 require 'controllers/forums_controller'
 require 'controllers/posts_controller'
